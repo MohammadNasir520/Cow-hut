cf3cc6f108efbb3d14b84e2f616a1a7ec6aed68b
MohammadNasir520 <nasirahsan520@gmail.com>
2023-07-03 07:15:20 +0600

    cheking commit

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -16,48 +16,9 @@ const createAdmin = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-// const getSingleAdmin = catchAsync(
-//   async (req: Request, res: Response, next: NextFunction) => {
-//     const { id } = req.params;
-//     const getAllAdmins = await AdminService.getSingleAdmin(id);
 
-//     sendResponse(res, {
-//       success: true,
-//       statusCode: httpStatus.OK,
-//       message: "Admin retrieved successfully",
-//       data: getAllAdmins,
-//     });
-//   }
-// );
-
-// const updateAdmin = async (req: Request, res: Response, next: NextFunction) => {
-//   const id = req.params.id;
-//   const updatedDAta = req.body;
-//   const updatedAdmin = await AdminService.updateAdmin(id, updatedDAta);
-
-//   sendResponse(res, {
-//     success: true,
-//     statusCode: httpStatus.OK,
-//     message: "Admin updated successfully",
-//     data: updatedAdmin,
-//   });
-// };
-
-// const deleteSingleAdmin = async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const deleteSingleAdmin = await AdminService.deleteSingleAdmin(id);
-
-//   sendResponse(res, {
-//     success: true,
-//     statusCode: 200,
-//     message: "Admin deleted successfully",
-//     data: deleteSingleAdmin,
-//   });
-// };
 
 export const AdminController = {
   createAdmin,
-  // getSingleAdmin,
-  // deleteSingleAdmin,
-  // updateAdmin,
+
 };
--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -9,44 +9,9 @@ const createAdmin = async (AdminData: IAdmin): Promise<Partial<IAdmin>> => {
   return others;
 };
 
-// const getAllAdmins = async () => {
-//   const getAllAdmins = await Admin.find({});
-//   return getAllAdmins;
-// };
-// const getSingleAdmin = async (id: string) => {
-//   const getAllAdmins = await Admin.findById(id);
-//   return getAllAdmins;
-// };
-// const deleteSingleAdmin = async (id: string) => {
-//   const deleteSingleAdmin = await Admin.findByIdAndDelete(id);
-//   return deleteSingleAdmin;
-// };
-// const updateAdmin = async (id: string, payload: Partial<IAdmin>) => {
-//   const { name, ...AdminData } = payload;
 
-//   const AdminUpdatedData = { ...AdminData };
-
-//   if (name && Object.keys(name).length > 0) {
-//     Object.keys(name).forEach((key) => {
-//       const nameKey = `name.${key}`;
-//       (AdminUpdatedData as any)[nameKey] = name[key as keyof typeof name];
-//     });
-//   }
-
-//   const updatedAdmin = await Admin.findOneAndUpdate(
-//     {
-//       _id: id,
-//     },
-//     AdminUpdatedData,
-//     { new: true }
-//   );
-//   return updatedAdmin;
-// };
 
 export const AdminService = {
   createAdmin,
-  // getAllAdmins,
-  // getSingleAdmin,
-  // deleteSingleAdmin,
-  // updateAdmin,
+
 };
